23f697e3297a56c7f8a45fde8addda38de63a61a
Kasia24 <160325853+Kasia24@users.noreply.github.com>
2024-07-20 21:03:36 +0200

    add commit

--- task2.js
+++ task2.js
@@ -1,5 +1,5 @@
 function calcAverageCalories(days) {
-  const totalCalories = days.reduce((total, day) => total + day.calories, 0);
+  const totalCalories = days.reduce((total, day) => total + day.calories);
   const averageCalories = totalCalories / days.length;
   return averageCalories;
 }
